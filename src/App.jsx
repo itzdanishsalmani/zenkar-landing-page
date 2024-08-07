08976e82c860e197144c8454595ec7dd0f1a63b6
Danish Salmani <salmanidanish488@gmail.com>
2024-08-07 17:35:39 +0530

    NavBar added

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
   return (
     <>
       <div >
-        {/* <NavBar /> */}
+        <NavBar />
         <MainCard />
         <Trusted />
         <VideoWithThreeCardSection />
--- src/components/Others/NavBar.jsx
+++ src/components/Others/NavBar.jsx
@@ -2,13 +2,22 @@ import { useState } from "react";
 import { Buttons } from "./Buttons";
 
 export function NavBar() {
+
+    const [isPhoneView, setPhoneView] = useState(false);
+
     const [isProductHovered, setIsProductHovered] = useState(false);
     const [isSolutionHovered, setIsSolutionHovered] = useState(false);
     const [ isResourcesHovered, setIsResourcesHovered ] = useState(false);
     const [ isCompanyHovered, setIsCompanyHovered ] = useState(false);
 
+    const [productOpen, setProductOpen] = useState(false);
+    const [companyOpen, setCompanyOpen] = useState(false);
+    const [solutionsOpen, setSolutionsOpen] = useState(false);
+    const [resourcesOpen, setresourcesOpen] = useState(false);
+
     return (
-        <div className="relative cursor-pointer">
+        <div >
+        <div className="web h-fit mt-12 hidden md:block relative cursor-pointer">
         <div className="fixed w-full h-16 bg-white border-b px-6 flex items-center justify-between">
             <img src="zenskarImg.svg" alt="" />
 
@@ -58,9 +67,138 @@ export function NavBar() {
                     text={"Book a Demo"}
                     image={"arrow_forward.svg"}
                 />
+                
             </div>
+            <div className="block md:hidden p-2 ml-2 md:mx-2 md:py-2 md:px-4 "
+                        onClick={() => setPhoneView(true)}>
+                        <img src="menu.svg" alt="Menu" />
+                        </div>
             </div>
         </div>
+         
+        {isPhoneView && (
+            <div>
+
+{/* Phone */}
+        <div className="phone z-10 bg-white w-screen">
+                <div className=" h-auto text-slate-500 cursor-pointer ">
+                    <div className="pt-4 px-4 ">
+                        <img src="zenskarImg.svg" alt="" />
+                        <img src="close.svg" alt="Closcursor-pointer"
+                            onClick={() => setPhoneView(false)} />
+
+                        <div className="menu mt-8 space-y-8 cursor-pointer">
+                            <div className="border-b border-slate-400">
+                                Why Zenkar
+                            </div>
+
+                            <div className="border-b border-slate-400"
+                                onClick={() => setProductOpen(!productOpen)} >
+                                <div className="flex justify-between">
+                                Product <img src="arrow_drop_down.svg" alt="" />
+                                </div>
+                                {productOpen && (
+                                    <div className="mt-4 space-y-4">
+                                        <div className="bg-slate-200">OFFERINGS</div>
+                                        <div>Billing</div>
+                                        <div>Usage Tracking</div> 
+                                        <div>Accounts Receivables</div> 
+                                        <div>Revenue Recognition</div> 
+                                        <div>Analytics</div>
+                                        <div>Integrations</div>
+
+                                        <div className="bg-slate-200">FEATURES</div>
+                                        <div>Contract Builder</div> 
+                                        <div>Customer Portal</div> 
+                                        <div>Entitlements</div>    
+                                    </div>
+                                )}
+                            </div>
+
+                            <div className="border-b border-slate-400"
+                                onClick={() => setSolutionsOpen(!solutionsOpen)}>
+                                    <div className="flex justify-between">Solutions
+                                        <img src="arrow_drop_down.svg" alt="" />
+                                    </div>
+                                {solutionsOpen && (
+                                    <div className="mt-4 space-y-4">
+                                        <div className="bg-slate-200">PRICING</div>
+                                        <div>Subscriptions</div>
+                                        <div>Usage based pricing </div>
+
+                                        <div className="bg-slate-200">ROLE</div>
+                                        <div>Finance</div>
+                                        <div>Product</div>
+                                        <div>Engineering</div>
+                                        <div>CEO & Founders</div>
+
+                                        <div className="bg-slate-200">INDUSTRIES</div>
+                                        <div>AI</div>
+                                        <div>Retail-tech</div>
+                                        <div>Fintech</div>
+                                        <div>Cloud Infrastructure</div>
+                                        <div>SaaS</div>
+                                        <div>E-learning</div>
+                                        <div>API</div>
+                                        <div>CPaaS</div>
+                                        <div>Dev tool</div> 
+                                    </div>
+                                )}
+                            </div>
+
+                            <div className="border-b border-slate-400"
+                                onClick={() => setresourcesOpen(!resourcesOpen)}>
+                                
+                                <div className="flex justify-between">Resources
+                                <img src="arrow_drop_down.svg" alt="" />
+                                </div>
+                                {resourcesOpen && (
+                                    <div className="mt-4 space-y-4">
+                                        <div className="bg-slate-200">RESOURCES</div>
+                                        <div>Blog</div>
+                                        <div>Webinar</div>
+                                        <div>Case Studies</div>
+                                        <div>Documentation</div>
+                                        <div>E-book</div>
+
+                                        <div className="bg-slate-200">COMPARE</div>
+                                        <div>Stripe</div>
+                                        <div>Chargebee</div>
+                                        <div>Maxio</div>
+
+                                        <div>The CFO's Guide to Future-Proofing Billing Workflow and Unlocking Business Growth</div>
+                                        <img src="resource.webp" alt="" />
+
+                                    </div>
+                                )}
+                            </div>
+
+                            <div className="border-b border-slate-400"
+                                onClick={() => setCompanyOpen(!companyOpen)}>
+                                <div className="flex justify-between">Company
+                                <img src="arrow_drop_down.svg" alt="" />
+                                </div>
+                                {companyOpen && (
+                                    <div className="mt-4 space-y-4">
+                                        <div>COMPANY</div>
+                                        <div>About us</div>
+                                        <div>Careers</div>
+                                    </div>
+                                )}
+                            </div>
+                            <div className="border-b border-slate-400">
+                                Pricing
+                            </div>
+                        </div>
+                        
+                       
+                        </div>
+                    </div>
+
+                </div>
+                </div>                
+)}
+            </div>
     )
 }
 
